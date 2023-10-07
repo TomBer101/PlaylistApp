62883d191f5250347361a2fd70c5f3b5a645f7c9
TomBer101 <74832275+TomBer101@users.noreply.github.com>
2023-10-07 19:58:38 +0300

    Delete mobileClient directory

--- mobileClient/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
